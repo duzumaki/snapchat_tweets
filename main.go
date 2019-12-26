20769ca416ba9a6e9287d6bd3498d67d1820dd6c
David Uzumaki <iamdaviduzumaki@gmail.com>
2019-12-26 04:56:03 +0000

    loop through tweets, extract ID and delete tweets

--- main.go
+++ main.go
@@ -1,39 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
+	"os"
 )
 
 func main() {
 
+	// Keys set as environment variables(for secruity puposes obviously)
 	consumerKey := os.Getenv("CONSUMER_ACCESS")
 	consumerSecret := os.Getenv("CONSUMER_SECRET")
 	accessToken := os.Getenv("ACCESS_TOKEN")
 	accessSecret := os.Getenv("ACCESS_SECRET")
 
+	// Oauth authetication
 	config := oauth1.NewConfig(consumerKey, consumerSecret)
 	token := oauth1.NewToken(accessToken, accessSecret)
 	httpClient := config.Client(oauth1.NoContext, token)
 
-	// // Twitter client
+	// Twitter client
 	client := twitter.NewClient(httpClient)
 
 	//get ID of user
 	userParams := &twitter.UserShowParams{ScreenName: "uzumakithegod"}
 	user, _, _ := client.Users.Show(userParams)
 
-	fmt.Println(user.ID)
+	// Set up to get access to tweets associated with User ID
+	tweets, _, _ := client.Timelines.UserTimeline(&twitter.UserTimelineParams{
+		UserID: user.ID,
+	})
 
-	// //loop through all Tweets
-	// tweet, _ := client.Timelines.UserTimeline(&twitter.UserTimelineParams{
-	// 	UserID:
-	// })
+	// loop through all tweets
+	for i := 0; i < len(tweets); i++ {
 
-	//	delete all tweets
-	//client.Statuses.Destroy(1209537877538988032, nil)
+		// Delete the current tweets iteration
+		client.Statuses.Destroy(tweets[i].ID, nil)
+	}
 
 }
